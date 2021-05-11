8708d42114a7bfa955c9729c5fb19a9d49be5d1b
Roberto Mauro <roberto.mauro@icloud.com>
2021-05-11 17:50:10 +0200

    Add Object.assign polyfill

--- js/indeterminatebar.js
+++ js/indeterminatebar.js
@@ -8,17 +8,55 @@
   }
 	
 })(this, function() {
-	// Create Element.remove() function if not exist in the browser
-	if (!('remove' in Element.prototype)) {
-	    Element.prototype.remove = function() {
-	        if (this.parentNode) {
-	            this.parentNode.removeChild(this);
-	        }
-	    };
+
+	/**
+	 * Inject polyfills for IE
+	 */
+	function createPolyfills() {
+		if (typeof Object.assign !== 'function') {
+		  // Must be writable: true, enumerable: false, configurable: true
+		  Object.defineProperty(Object, "assign", {
+		    value: function assign(target, varArgs) { // .length of function is 2
+		      'use strict';
+		      if (target === null || target === undefined) {
+		        throw new TypeError('Cannot convert undefined or null to object');
+		      }
+
+		      var to = Object(target);
+
+		      for (var index = 1; index < arguments.length; index++) {
+		        var nextSource = arguments[index];
+
+		        if (nextSource !== null && nextSource !== undefined) { 
+		          for (var nextKey in nextSource) {
+		            // Avoid bugs when hasOwnProperty is shadowed
+		            if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+		              to[nextKey] = nextSource[nextKey];
+		            }
+		          }
+		        }
+		      }
+		      return to;
+		    },
+		    writable: true,
+		    configurable: true
+		  });
+		}
+
+		// Create Element.remove() function if not exist in the browser
+		if (!('remove' in Element.prototype)) {
+		    Element.prototype.remove = function() {
+		        if (this.parentNode) {
+		            this.parentNode.removeChild(this);
+		        }
+		    };
+		}
 	}
 
+	createPolyfills()
+
 	// setInterval ID used to check on the loading progress percent
-	var progressIntervalID = null;
+	var progressIntervalID = null
 
 	// Prepares the HTML dom elements for the progress bar
 	var slider = document.createElement('div');
